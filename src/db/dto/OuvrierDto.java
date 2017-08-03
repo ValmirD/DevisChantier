124540672b31e1b51fce47a3d638b478a35462d8
Valmir Demaj <giti-adidas@hotmail.com>
2017-08-03 11:39:25 +0200

    dto

---
+++ src/db/dto/OuvrierDto.java
@@ -0,0 +1,15 @@
+package db.dto;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Vali
+ */
+public class OuvrierDto extends EntityDto<Integer> {
+    
+}
